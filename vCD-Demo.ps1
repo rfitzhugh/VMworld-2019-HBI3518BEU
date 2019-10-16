a8eb0837b22286c1649d9fdfe2180dbd5a411d99
Rebecca <rmfitzhugh@gmail.com>
2019-10-16 17:34:07 +0800

    Update vCD-Demo.ps1

--- vCD-Demo.ps1
+++ vCD-Demo.ps1
@@ -1,3 +1,6 @@
+Import-Module .\Rubrik -Force
+# Force import Rubrik module.
+
 Connect-Rubrik -Server 192.168.150.121 -Username rebecca@rubrik.us
 # This will connect to Rubrik with a username of "admin" to the IP address.
 # The prompt will request a secure password.      
@@ -5,10 +8,10 @@ Connect-Rubrik -Server 192.168.150.121 -Username rebecca@rubrik.us
 Get-RubrikVCD -Status 'Connected'
 # This returns the vCD settings on the currently connected Rubrik cluster with the status of 'Connected'
 
-Get-RubrikVApp -Name 'vApp01'
+Get-RubrikVApp -Name 'Demo-vApp01' -PrimaryClusterID local
 # This returns details on all vCD vApps named "vApp01".
 
-Get-RubrikVApp "vApp1" | Protect-RubrikVApp -SLA 'Gold'
+Get-RubrikVApp "Demo-vApp01" -PrimaryClusterID local | Protect-RubrikVApp -SLA 'Gold'
 # This will assign the Gold SLA Domain to any vApp named "vApp1"
 
 Get-RubrikSnapshot -id 'VcdVapp:::01234567-8910-1abc-d435-0abc1234d567'
@@ -26,7 +29,7 @@ $vm = @()
 $vm += $restorableVms[0]
 Export-RubrikVApp -id $vapp.id -snapshotid $snapshot.id -Partial $vm -ExportMode ExportToTargetVapp -PowerOn:$false
 
-This retreives the latest snapshot from the given vApp 'vApp01' and perform a partial export on the first VM in the vApp.
+This retrieves the latest snapshot from the given vApp 'vApp01' and perform a partial export on the first VM in the vApp.
 The VM is exported into the existing parent vApp. Set the ExportMode parameter to 'ExportToNewVapp' parameter to create a new vApp for the partial export.
 This is an advanced use case and the user is responsible for parsing the output from Get-RubrikVApp, or gathering data directly from the API.
 Syntax of the object passed with the -Partial Parameter is available in the API documentation.

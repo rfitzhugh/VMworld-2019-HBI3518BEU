d99f772a7a4eb55df6a523e30e60281d956b72a6
Rebecca <rmfitzhugh@gmail.com>
2019-11-04 11:34:59 +0100

    updates for vmworld

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # VMworld-2019-HBI3518BEU
- Code for vCloud Director PowerShell demo.
+Code for vCloud Director PowerShell demo.
+
+This demonstration includes the following: 
+1. Connecting to a Rubrik cluster
+2. Verifying connection to a vCloud Director instance
+3. Assigning an SLA Domain to a vApp
+4. Taking an on-demand snapshot of a vApp
+5. Exporting a vApp to the same organization
+
+Email build@rubrik.com if you have any questions. 
--- vCD-Demo.ps1
+++ vCD-Demo.ps1
@@ -1,18 +1,21 @@
 Import-Module .\Rubrik -Force
 # Force import Rubrik module.
 
-Connect-Rubrik -Server 192.168.150.121 -Username rebecca@rubrik.us
+Connect-Rubrik -Server 0.0.0.0 -Username rebecca@rubrik.us
 # This will connect to Rubrik with a username of "admin" to the IP address.
 # The prompt will request a secure password.      
       
 Get-RubrikVCD -Status 'Connected'
 # This returns the vCD settings on the currently connected Rubrik cluster with the status of 'Connected'
 
-Get-RubrikVApp "demo-lin" -PrimaryClusterID local | Protect-RubrikVApp -SLA 'Gold'
-# This will assign the Gold SLA Domain to any vApp named "vApp1"
+Get-RubrikVApp "demo-lin" -PrimaryClusterID local | Protect-RubrikVApp -SLA 'VMworld-Demo'
+# This will assign the VMworld Demo SLA Domain to any vApp named "demo-lin"
+
+Get-RubrikVApp "demo-lin" -PrimaryClusterID local | New-RubrikSnapshot -SLA 'Maint-12H-R07'
+# This will take an on-demand snapshot of "demo-lin" and retain it per the maintenance SLA policy.
 
 Get-RubrikVApp -Name 'Demo-vApp01' -PrimaryClusterID local
-# This returns details on all vCD vApps named "vApp01".
+# This returns details on all vCD vApps named "Demo-vApp01".
 
 Get-RubrikSnapshot -id 'VcdVapp:::01234567-8910-1abc-d435-0abc1234d567' 
 # This will return all snapshot (backup) data for the virtual machine id of "VirtualMachine:::aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee-vm-12345"
